9dbf9f58815a51fdccc5352b7e3da5899addb2fc
= <luz.matheus11@gmail.com>
2021-09-17 20:50:49 -0300

    Req 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './Componentes/Album';
 import Login from './Componentes/Login';
 import Search from './Componentes/Search';
@@ -12,13 +12,15 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" exact component={ Login } />
-        <Route path="/search" exact component={ Search } />
-        <Route path="/Album/:id" exact component={ Album } />
-        <Route path="/Favorites" exact component={ Favorites } />
-        <Route path="/Profile/edit" exact component={ ProfileEdit } />
-        <Route path="/Profile" exact component={ Profile } />
-        <Route path="*" exact component={ NotFound } />
+        <Switch>
+          <Route path="/search" exact component={ Search } />
+          <Route path="/Album/:id" exact component={ Album } />
+          <Route path="/Favorites" exact component={ Favorites } />
+          <Route path="/Profile/edit" exact component={ ProfileEdit } />
+          <Route path="/Profile" exact component={ Profile } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/Componentes/Login.js
+++ src/Componentes/Login.js
@@ -1,31 +1,82 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonHabition: false,
+      nameUser: '',
+      loading: false,
+      redirectLogin: false,
+    };
+  }
+
+  handleChange = (login) => {
+    const THREE = 3;
+    const validation = login.length >= THREE;
+    if (validation) {
+      this.setState({
+        buttonHabition: true,
+      });
+    }
+    this.setState({
+      nameUser: login,
+    });
+  }
+
+  buttonClick = async () => {
+    this.setState({
+      loading: true,
+    });
+    const { nameUser } = this.state;
+    await createUser({ name: nameUser });
+    this.setState({
+      loading: false,
+      redirectLogin: true,
+    });
+  }
+
+  submitClick = (event) => {
+    event.preventDefault();
+  }
+
   render() {
+    const { buttonHabition, nameUser, loading, redirectLogin } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (redirectLogin) {
+      return <Redirect to="/search" />;
+    }
+
     return (
       <div data-testid="page-login">
-        <form>
+        <form onSubmit={ this.submitClick }>
           <label htmlFor="login">
             Nome:
             <input
+              data-testid="login-name-input"
               type="text"
+              name="nameUser"
+              value={ nameUser }
               id="login"
+              onChange={ ({ target }) => this.handleChange(target.value) }
             />
           </label>
-          <label htmlFor="email">
-            Email:
-            <input
-              type="email"
-              id="email"
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              type="password"
-              id="password"
-            />
-          </label>
+          <button
+            data-testid="login-submit-button"
+            type="submit"
+            disabled={ !buttonHabition }
+            onClick={ this.buttonClick }
+          >
+            Entrar
+          </button>
         </form>
       </div>
 
