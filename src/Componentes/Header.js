20cd1ec5a5ef7616f8a51193b0866865697793d7
= <luz.matheus11@gmail.com>
2021-09-17 23:06:47 -0300

    corrigindo lint

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
-import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor() {
