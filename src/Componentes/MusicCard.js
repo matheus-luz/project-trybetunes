8a4a6fffee6beb9f121575294b01f28d405ddc87
= <luz.matheus11@gmail.com>
2021-09-21 21:51:51 -0300

    Req 8

--- src/Componentes/MusicCard.js
+++ src/Componentes/MusicCard.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      songFavorite: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.musicsFavorits();
+  }
+
+  componentDidUpdate() {
+    this.musicsFavorits();
+  }
+
+  favoriteMusics = async (event, music) => {
+    const valueCheck = event.target.checked;
+    if (valueCheck) {
+      this.setState({
+        loading: true,
+      });
+      await addSong(music);
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  musicsFavorits = async () => {
+    const songGet = await getFavoriteSongs();
+    console.log(songGet);
+    this.setState({
+      songFavorite: songGet,
+    });
+  }
+
   render() {
     const { musics } = this.props;
+    const { loading, songFavorite } = this.state;
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
       <div>
         <form>
           { musics.slice(1).map((music, index) => (
             <label
-              data-testid={ `checkbox-music-${musics.trackId}` }
-              htmlFor="checkbox-input"
+              data-testid={ `checkbox-music-${music.trackId}` }
+              htmlFor={ music.trackId }
               key={ index }
             >
               <span>
@@ -31,9 +75,13 @@ class MusicCard extends React.Component {
                   { music.artistViewUrl }
                 </code>
               </audio>
+              Favorite:
               <input
+                name={ music.trackId }
                 type="checkbox"
-                id="checkbox-input"
+                id={ music.trackId }
+                checked={ songFavorite.some((song) => song.trackId === music.trackId) }
+                onChange={ (event) => this.favoriteMusics(event, music) }
               />
               <div />
             </label>
